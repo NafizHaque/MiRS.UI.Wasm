d242b2f73ed7c75fbf7823fba44dfb42cb8277a4
AardentNafiz <92028026+NafizHaque@users.noreply.github.com>
2025-03-09 20:04:32 +0000

    navbar additions

--- MiRS.UI.Wasm/Program.cs
+++ MiRS.UI.Wasm/Program.cs
@@ -1,18 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace MiRS.UI.Wasm;
-
-public class Program
+namespace MiRS.UI.Wasm
 {
-    public static async Task Main(string[] args)
+    public class Program
     {
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
-        builder.RootComponents.Add<HeadOutlet>("head::after");
+        public static async Task Main(string[] args)
+        {
+            WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-        await builder.Build().RunAsync();
+            await builder.Build().RunAsync();
+        }
     }
 }
